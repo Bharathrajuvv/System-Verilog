ec1a1899087c85d5b2168bcee913c3d93da59dee
Bharathrajuvv <rajubharath542@gmail.com>
2025-07-07 14:36:53 +0530

    Create 04.bit.sv

---
+++ Data types/02. 2 State/04.bit.sv
@@ -0,0 +1,13 @@
+module tb;
+  bit   [3:0] var_b;    
+  logic [3:0] x_val;   
+
+  initial begin
+    x_val = 4'b01zx;
+    var_b = x_val;
+    $display ("x_val=%b   var_b = %b", x_val, var_b);
+  end
+endmodule
+
+Output:
+# KERNEL: x_val=01zx   var_b = 0100
