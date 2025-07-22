fb66acdc354e9c2d9d40f3367913158d7d73c2e4
Bharathrajuvv <rajubharath542@gmail.com>
2025-07-22 19:49:35 +0530

    Create 01. Single dimensional .sv

---
+++ Data types/04. Arrays/01.Dimensional array/01. Single dimensional .sv
@@ -0,0 +1,19 @@
+// Code your testbench here
+// or browse Examples
+module single_dime;
+  int array[5] = '{10,20,30,40,50};
+  initial begin
+    foreach (array[i])begin
+      $display("array[%0d]=%0d",i,array[i]);
+    end
+  end
+endmodule
+
+Output:
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jul 22 10:19 2025
+array[0]=10
+array[1]=20
+array[2]=30
+array[3]=40
+array[4]=50
+           V C S   S i m u l a t i o n   R e p o r t 
