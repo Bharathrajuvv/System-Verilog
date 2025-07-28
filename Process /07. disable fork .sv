d0af208f37ebab839e1f2ebce4856b4b3091d0fa
Bharathrajuvv <rajubharath542@gmail.com>
2025-07-28 20:10:32 +0530

    Create 07. disable fork .sv

---
+++ Process /07. disable fork .sv
@@ -0,0 +1,38 @@
+module disable_fork_example;
+  initial begin
+    fork
+      begin 
+        $display("Process A started at time = %0t", $time);
+        #10;
+        $display("Process A completed at time = %0t", $time);
+      end
+      begin
+        $display("Process B started at time = %0t", $time);
+        #15;
+        $display("Process B completed at time = %0t", $time);
+      end
+      begin 
+        $display("Process C started at time = %0t", $time);
+        #20;
+        $display("Process C completed at time = %0t", $time);
+      end
+    join_any
+    disable fork;
+    $display("fork-join_any completed at time = %0t", $time);
+  end
+endmodule
+
+Output:
+ Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jul 28 10:39 2025
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+fork-join_any completed at time = 10
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 10 ns     
+
+
+
+      
