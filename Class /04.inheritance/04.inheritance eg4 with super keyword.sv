69ca581dcfb666f10a2d87428e14dc0bfd79320e
Bharathrajuvv <rajubharath542@gmail.com>
2025-09-26 20:06:13 +0530

    Create 04.inheritance eg4 with super keyword.sv

---
+++ Class /04.inheritance/04.inheritance eg4 with super keyword.sv
@@ -0,0 +1,33 @@
+// Code your testbench here
+// or browse Examples
+class x;
+  int ands,ors;
+  function cal(int a,b);
+    ands = a&b;
+    ors = a|b;
+  endfunction
+endclass
+
+class y extends x;
+  int xors;
+  function cal(int a,b);
+    super.cal(a,b);
+    xors=a^b;
+  endfunction
+endclass
+
+module class_eg;
+  initial begin
+    y c1;
+    c1=new();
+    c1.cal(13,14);
+    $display("Value of ands=%0b ors=%0b xors=%0b",c1.ands,c1.ors,c1.xors);
+  end
+endmodule
+
+
+Output:
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Sep 26 10:35 2025
+Value of ands=1100 ors=1111 xors=11
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
