7b001b33ca543c0b1caca63e62bbe20cd4dd48aa
Bharathrajuvv <rajubharath542@gmail.com>
2025-10-03 18:56:59 +0530

    Create 06.gobal task.sv

---
+++ Task/06.gobal task.sv
@@ -0,0 +1,18 @@
+// Code your testbench here
+// or browse Examples
+module inc;
+  initial begin
+    dis();
+  end
+endmodule
+
+task dis;
+  $display("HI BHARATH");
+endtask
+
+
+Output:
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct  3 09:26 2025
+HI BHARATH
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
